4fe82432ba1e937d8a9f4c412d26635866c09e19
jswright86 <jwright9@byupathway.edu>
2025-03-10 21:31:14 -0500

    commit

---
+++ images/math.py
@@ -0,0 +1,18 @@
+#Prompt 1
+
+age = input(f"How old are you? ")
+next_birthday = int(age) + 1
+print(f"On your next birthday, you will be {next_birthday} years old.")
+
+#Prompt 2
+
+egg_cartons = input(f"How many egg cartons do you have? ")
+total_eggs = int(egg_cartons) * 12
+print(f"You have {total_eggs} eggs.")
+
+#Prompt 3
+
+cookies = input(f"How many cookies are there? ")
+people = input(f"How many people are there? ")
+cookies_per_person = int(cookies) / int(people)
+print(f"Each person gets {cookies_per_person} cookies.")
